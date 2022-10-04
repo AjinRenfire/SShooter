905186c3cb1745697e000d677dd79f729d39f181
AjinRenfire <ajinra4@outlook.com>
2022-10-04 19:02:48 +0530

    player & enemy  added ,  movement for player added

---
+++ src/index.js
@@ -0,0 +1,44 @@
+//player control
+
+
+/******* initial variables  *******/
+var isRunning = true;
+var inputKey = "";
+const SPEED = 10;
+const PLAYER = document.getElementById("player");
+const SPACE = document.getElementById("space");
+PLAYER.x = PLAYER.offsetLeft;
+PLAYER.y = PLAYER.offsetTop;
+/* *********************************** */
+
+function shootAmmo(){
+    const ammo = document.createElement("div");
+    ammo.setAttribute("id","ammo");
+
+}
+
+function getInput(){
+    document.onkeydown = getKey; // getKey is a function
+
+}
+
+function getKey(event){
+    event = (event)?event : window.event;
+    switch(event.key){
+        case "ArrowLeft":
+            PLAYER.x+=(-SPEED);
+            PLAYER.style.left = PLAYER.x+"px";
+            break;
+        case "ArrowRight":
+            PLAYER.x+=SPEED;
+            PLAYER.style.left = PLAYER.x+"px";
+            break;
+        case "ArrowUp":
+            shootAmmo();
+            break;
+    }
+}
+
+getInput();
+
+
